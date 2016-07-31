ecc93e32878196636283c07c36b3643f37373605
kaixuan1995 <2534189166@qq.com>
2016-07-31 20:55:55 +0800

    在AfterAdvice中获取目标方法中的参数

--- src/com/abc/advice/AfterAdviceTest.java
+++ src/com/abc/advice/AfterAdviceTest.java
@@ -5,8 +5,8 @@ import org.aspectj.lang.annotation.Aspect;
 
 @Aspect
 public class AfterAdviceTest {
-    @After(value="execution(* com.abc.service.*.afterAdvice*(..))")
-    public void releaseResource() {
+    @After(value="execution(* com.abc.service.*.afterAdvice*(..)) && args(name)")
+    public void releaseResource(String name) {
         System.out.println("模拟释放数据库连接");
     }
 }
